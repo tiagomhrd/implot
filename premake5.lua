e25fc00238bb7de9a9fae6b27c039f6609468e8f
Tiago Moherdaui <tiagomoherdaui@gmail.com>
2022-09-15 13:32:06 -0300

    premake

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+project "ImPlot"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"implot.h",
+		"implot.cpp",
+		"implot_demo.cpp",
+		"implot_internal.h",
+		"implot_items.cpp",
+	}
+
+	includedirs
+	{
+		"../ImGUI",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
